3a8525d93854685786e9e36687a492cead4b2001
Berke Bozacı <bbozaci20@ku.edu.tr>
2024-10-21 23:39:40 +0300

    Initial Commit

---
+++ atehere/atehereApp.swift
@@ -0,0 +1,17 @@
+//
+//  atehereApp.swift
+//  atehere
+//
+//  Created by Berke Bozacı on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct atehereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
