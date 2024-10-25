8962a0e842bf15f8c8f90b310d110b1edc2d2b5d
Berke Bozacı <bbozaci20@ku.edu.tr>
2024-10-25 19:17:52 +0300

    Firebase added

---
+++ atehere/Models/Customer.swift
@@ -0,0 +1,15 @@
+//
+//  Customer.swift
+//  atehere
+//
+//  Created by Berke Bozacı on 25.10.2024.
+//
+
+import Foundation
+
+struct Customer: Codable, Identifiable {
+    var id: String
+    var name: String
+    var surname: String
+    
+}
